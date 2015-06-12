4800623c05ef4b4692378329d7c3398fd979e69f
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-12 14:57:24 -0400

    added Answer model tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Answer do
+  let(:answer) {FactoryGirl.build(:answer)}
+
+  it 'is valid with a body' do
+    expect(answer).to be_valid
+  end
+
+  it 'is invalid without a body' do
+    answer2 = Answer.new(body: nil)
+    answer2.valid?
+    expect(answer2.errors[:body]).to include("can't be blank")
+  end
 end
