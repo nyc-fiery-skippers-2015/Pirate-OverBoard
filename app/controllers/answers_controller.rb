1655964bff4317a6f2edc5ff274efd1fc8317016
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-12 15:38:50 -0400

    Added Answers#create method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,4 +3,21 @@ class AnswersController < ApplicationController
     @question = Question.find_by(id: params[:question_id])
     @answer = Answer.new
   end
+
+  def create
+    @question = Question.find_by(id: params[:question_id])
+    @answer = @question.answers.build(answers_params)
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def answers_params
+    params.require(:answer).permit(:body, :user_id)
+  end
 end
