51bbb08557780a738206760eb682b7729fdcd7ae
Don <dholness2@gmail.com>
2015-06-12 15:15:50 -0400

    usertest validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it "User should be a uniqu user" do
+    user_one = create(:user)
+    user_two = build(:user, username:user_one.username, email:user_one.email)
+    user_two.valid?
+    expect(user_two.erros[:username]).to include('has already been taken')
+  end
+  it "is invalid without an email address"
+  it "user should have a pasword"
+
 end
