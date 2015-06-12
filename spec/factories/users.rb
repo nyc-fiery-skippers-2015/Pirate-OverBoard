ffd624340eecbdd28b531679dbab5920554d40d9
Don <dholness2@gmail.com>
2015-06-12 15:14:11 -0400

    userfactory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    username {Faker::Internet.user_name}
+    email {Faker::Internet.email}
+    password_digest {Faker::Internet.password(6)}
   end
 
 end
