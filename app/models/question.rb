ec0f6fd1eabb0533aac5b41395314b7238aca144
Mendel Schneerson <mmschneerson@gmail.com>
2015-06-12 17:58:49 -0400

    working on question sorting

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  after_save :default_vote
+
   belongs_to :user
   has_many :answers
   has_many :votes, as: :votable
@@ -6,4 +8,15 @@ class Question < ActiveRecord::Base
 
   validates_presence_of :title, :body
 
+
+  def default_vote
+    vote = self.votes.build(vote_count:0)
+    vote.user = current_user
+    vote.save
+  end
+
 end
+
+
+
+ # Question.joins(:votes).group(:id).order('SUM(votes.vote_count) DESC')
