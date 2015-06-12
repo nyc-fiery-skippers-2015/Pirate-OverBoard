3ef0fc1f220d15e82619e6820c51a288bb113ef5
Mendel Schneerson <mmschneerson@gmail.com>
2015-06-12 16:35:51 -0400

    adds al methods for QuestionController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,45 @@ class QuestionsController < ApplicationController
     @question = Question.find_by(id: params[:id])
   end
 
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    #@question.user = current_user
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @question = Question.find_by(id: params[:id]) 
+  end
+
+
+  def update
+    @question = Question.find_by(id: params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end 
+  end
+
+  def destroy
+    @question = Question.find_by(id: params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
+
 end
