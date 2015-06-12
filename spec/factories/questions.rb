b098183680847996398301bf6961248c47d3cd53
Mendel Schneerson <mmschneerson@gmail.com>
2015-06-12 15:31:44 -0400

    adds factory for questions controller specs

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
   factory :question do
-    
+    title { Faker::Lorem.sentence }
+    body { Faker::Lorem.sentence }
+    user
+  end
+
+  factory :user do
+    user_name { Faker::Internet.user_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 
 end
