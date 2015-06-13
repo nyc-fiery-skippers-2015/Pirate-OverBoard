6735bf35eb3af69ccb1662b88e0054f001a429cf
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-13 11:55:58 -0400

    bug fixes to make front end work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers, except: [:index, :show]
   end
+  resources :users, :sessions
+
+  get 'welcome/index'
+  get 'login', to: 'sessions#new', as: :login
+  post 'login' => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  get 'signup', to: 'users#new', as: :signup
+  post 'signup' => 'users#create'
+  root 'welcome#index'
 end
