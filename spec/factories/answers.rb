18058092716c8b283558a053b6a494150366da01
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-14 12:35:57 -0400

    add Comments and Votes tests for Answers Model Tests

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -5,4 +5,16 @@ FactoryGirl.define do
     question
   end
 
+  factory :answer_comment, class: "Comment" do
+    association :commentable, factory: :answer
+    body { Faker::Lorem.sentence }
+    user
+  end
+
+  factory :answer_vote, class: "Vote" do
+    association :votable, factory: :answer
+    vote_count{1}
+    user
+  end
+
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,13 +3,36 @@ require 'rails_helper'
 describe Answer do
   let(:answer) {FactoryGirl.build(:answer)}
 
-  it 'is valid with a body' do
-    expect(answer).to be_valid
-  end
+  subject{ answer }
+  it { should be_valid }
 
   it 'is invalid without a body' do
     answer2 = Answer.new(body: nil)
     answer2.valid?
     expect(answer2.errors[:body]).to include("can't be blank")
   end
+end
+
+describe 'Answer Comments' do
+  let(:answer) { FactoryGirl.build(:answer) }
+  let(:answer_comment) { FactoryGirl.create(:answer_comment, commentable: answer) }
+
+  subject{ answer_comment }
+
+  it { should be_valid }
+
+  it { should respond_to(:body) }
+  it { should respond_to(:commentable) }
+end
+
+describe 'Answer Votes' do
+  let(:answer) { FactoryGirl.build(:answer) }
+  let(:answer_vote) { FactoryGirl.create(:answer_vote, votable: answer) }
+
+  subject{ answer_vote }
+
+  it { should be_valid }
+
+  it { should respond_to(:vote_count) }
+  it { should respond_to(:votable) }
 end
