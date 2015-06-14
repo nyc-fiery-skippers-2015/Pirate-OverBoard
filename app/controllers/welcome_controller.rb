5208dce45fc3b9c8c06ee6e355c5b92ef29d2971
Don <dholness2@gmail.com>
2015-06-14 15:05:58 -0400

    stats update to view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
-  
+
   def index
    @questions =Question.all
+   @question_votes =  Vote.where(votable_type: 'Question').group(:votable_id).sum(:vote_count)
   end
-  
+
 end
 
