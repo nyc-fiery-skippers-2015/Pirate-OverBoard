67a639fc2449752d73280ab943ab627725bbe8ab
Mendel Schneerson <mmschneerson@gmail.com>
2015-06-14 13:01:49 -0400

    auth helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
+
+  def logged_in?
+    current_user !=nil
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in do this action"
+      redirect_to login_url 
+    end
+  end
+
 end
